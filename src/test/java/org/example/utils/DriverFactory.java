edc481d8cc3b47438cb37e795833416aa4ea47e1
hp <prakashpatel.tester@gmail.com>
2026-05-15 09:00:51 +0530

    Remove Extra Code

--- src/test/java/org/example/utils/DriverFactory.java
+++ src/test/java/org/example/utils/DriverFactory.java
@@ -37,34 +37,4 @@ public class DriverFactory {
     }
 }
 
-//****Exsting File****//
-//package org.example.utils;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//
-//public class DriverFactory {
-//    private static WebDriver driver;
-//
-//    public static WebDriver getDriver() {
-//        if (driver == null) {
-//            WebDriverManager.chromedriver().setup();
-//            ChromeOptions options = new ChromeOptions();
-//            // Enable UI mode (non-headless)
-//            options.addArguments("--no-sandbox");
-//            options.addArguments("--disable-dev-shm-usage");
-//            options.addArguments("--start-maximized");
-//            driver = new ChromeDriver(options);
-//        }
-//        return driver;
-//    }
-//
-//    public static void quitDriver() {
-//        if (driver != null) {
-//            driver.quit();
-//            driver = null;
-//        }
-//    }
-//}
+
